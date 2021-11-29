fe59a158f5b682b08100c42c29103940615007b7
José Antonio <delosangelesrodriguezjose@gmail.com>
2021-11-29 13:41:56 +0100

    reto perfiles

---
+++ microserviciotest/src/main/java/com/everis/bootcamp/MicroserviciotestApplication.java
@@ -0,0 +1,13 @@
+package com.everis.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciotestApplication.class, args);
+	}
+
+}
